16af51f571fc3c7a12575b9a53c41a1663a2f1c6
Shengzhe Chen <shengzhe.chen@gmail.com>
2021-03-06 00:12:24 -0800

    update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'df715c3baf55711e20ed2a043e96436cf0d6a5b9',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'df715c3baf55711e20ed2a043e96436cf0d6a5b9',
+    ),
+    'php-curl-class/php-curl-class' => 
+    array (
+      'pretty_version' => '8.9.0',
+      'version' => '8.9.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a40cc77eb45bc6489240415197734ca747ce4820',
+    ),
+  ),
+);
